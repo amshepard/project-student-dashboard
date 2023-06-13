df36abbfb921765281ebed34190ded6ee604ed16
amshepard <antoineshepard@pursuit.org>
2023-06-13 19:19:47 -0400

    Working on functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
-import StudentList from "./components/StudentList";
 import CohortList from "./Components/CohortList";
+import StudentList from "./Components/StudentList";
 import data from "./data/data.json";
 
 function App() {
-   
+  const [studentList, setStudentList] = useState(data);
+  const [typeCohort, setTypeCohort] = useState("All Students")
+
+   function displayAllStudents() {
+    setTypeCohort("All Students")
+   setStudentList((studentList) => [...data]);
   }
-  
+
+    function sortCohort(cohortCode) {
+    setStudentList(data.filter(student => student.cohort.cohortCode === cohortCode.split(" ").join("")))
+    setTypeCohort(cohortCode)
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Student Dashboard</h1>
+      </header>
+      {}
+      <CohortList displayAllStudents={displayAllStudents} sortCohort={sortCohort}/>
+      <StudentList studentList={studentList} typeCohort={typeCohort}/>
+    </div>
+  );
+}
+
+export default App;
